e268cf4c43c162f88f498347cfdaad8d79e85bd3
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-21 18:43:59 -0600

    added library with add,update, and view js files

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // packages needed for application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateSQL = require('./utils/generateSQL');
+const generateSQL = require('./lib/generateSQL');
 
+// array of application options
+const options = [
+    'View Departments',
+    'Add a Department',
+    new inquirer.Separator(),
+    'View Roles',
+    'Add a Role',
+    new inquirer.Separator(),
+    'View Employees',
+    'Add an Employee',
+    'Update an Employee Role'
+];
+
+// function to initialize app
+function init() {
+
+
+    // inquirer prompt method to ask user input
+    inquirer.prompt(
+        {
+            type:'choices',
+            name:'options',
+            message:'Select an Option:',
+            choices: options
+        }
+    )
+    .then((data)=>{
+
+    });
+}
+
+init();
