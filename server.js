ef47b27c42726c69502ec0e03ff5e7abf10735b8
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-22 18:26:04 -0600

    updated add and view options

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function run() {
             }
         })
         .catch((err) =>{
-            console.err(err);
+            console.log(err);
             console.log('Something went wrong.');
         });
 }
@@ -62,8 +62,71 @@ function view(selection){
 }
 
 function add(selection){
-    const addEl = new Add(selection,123);
-    console.log(addEl);
+    switch(selection){
+        case 'department':
+            inquirer.
+                prompt(
+                    [
+                        {
+                            type:'input',
+                            name: 'department',
+                            message: 'Input department name:',
+                        },
+                    ],
+                )
+                .then((data) => {
+                    const addEl = new Add(selection,[data.department]);
+                    db.query(addEl.renderSQL(), (err) => {
+                        if (err) console.log('Error',err);
+                    });
+                    run();
+                })
+                .catch((err) =>{
+                    console.log(err);
+                    console.log('Something went wrong.');
+                });
+            break;
+        case 'role':
+            inquirer.
+                prompt(
+                    [
+                        {
+                            type:'input',
+                            name: 'role',
+                            message: 'Input role name:',
+                        },
+                        {
+                            type:'number',
+                            name:'salary',
+                            message:'Input salary:',
+                        },
+                        {
+                            type:'number',
+                            name:'department',
+                            message:'Input an department id',
+                        }
+                    ],
+                )
+                .then((data) => {
+                    const addEl = new Add(selection,[data.departmentName]);
+                    db.query('SELECT * FROM department;', (err,res) => {
+                        if (err) console.log('Error',err);
+                        console.table(res);
+                    });
+                    
+                    run();
+                })
+                .catch((err) =>{
+                    console.log(err);
+                    console.log('Something went wrong.');
+                });
+            break;
+
+        default: break;
+    }
+    // const addEl = new Add(selection,123);
+    // console.log(addEl);
+
 }
 
 function update(){
@@ -78,9 +141,9 @@ function endSession(){
 
 db.connect((err)=> {
     if(err){
-        console.error('Error:',err);
+        console.log('Error:',err);
     } else {
-    console.log('Connection to db established.');
+    console.log('Connection to database established.');
     run();
     }
 });
